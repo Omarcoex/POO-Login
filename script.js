f929ae230d8341d98cf3e82a67c76c277388c04f
Omar Aleiser Alvarez Laverde <omaraleyser@hotmail.com>
2022-10-14 13:24:09 -0500

    Se agrega funcion

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ class Interfaz {
     </div>
   </div>
 </div>`;
+console.log('hollllaaaaaaaaa');
     list.appendChild(elemento);
     this.dejarblanco();
   }
@@ -25,25 +26,45 @@ class Interfaz {
     document.getElementById("personas-form").reset();
   }
   eliminarpersona(elemento) {
-    if (elemento.name == "eliminar") {
-      elemento.parentElement.parentElement.remove();
-    }
+    elemento.parentElement.parentElement.parentElement.remove();
+  }
+  mostrarmensaje(mensaje, tipo) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${tipo}`;
+    div.appendChild(document.createTextNode(mensaje));
+    const contenedor = document.getElementsByClassName("container");
+    const app = document.getElementById("app");
+    contenedor[0].insertBefore(div, app);
+    setTimeout(function () {
+      document.querySelector(".alert").remove();
+      document.querySelector(".btn").disabled = false;
+    }, 2000);
   }
 }
 // elemento que interactua con el dom html
+// instanciamos con dom element
 document.getElementById("personas-form").addEventListener("submit", (e) => {
   e.preventDefault();
   const nombre = document.getElementById("nombre").value;
   const apellido = document.getElementById("apellido").value;
   const documento = document.getElementById("documento").value;
+  // document.querySelector(".btn").disabled = true;
 
-  // instanciamos
-  const persona = new Persona(nombre, apellido, documento);
-  const interfaz = new Interfaz();
   if (nombre === "" || apellido === "" || documento === "") {
-    alert("Campos vacios");
+    const interfaz = new Interfaz();
+    // alert("Campos vacios");
+    interfaz.mostrarmensaje("Campos vacios",'warning');
   } else {
+    const persona = new Persona(nombre, apellido, documento);
+    const interfaz = new Interfaz();
     interfaz.anadirpersona(persona);
-    alert("Persona creada Correctamente")
+    interfaz.mostrarmensaje("Persona agregada Correctamente",'warning');
+  }
+});
+document.getElementById(".personas-list").addEventListener("click", (e) => {
+  if (e.target.name == "eliminar") {
+    const interfaz = new Interfaz();
+    interfaz.eliminarPersona(e.target);
+    interfaz.mostrarmensaje("Elemento eliminado", "danger");
   }
 });
